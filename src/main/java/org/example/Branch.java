02c50090c170b12fa984ccea93c75de678c62b2e
paulinaboron <paulinaboron04@gmail.com>
2025-03-15 19:07:06 +0100

    first commit

---
+++ src/main/java/org/example/Branch.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Branch {
+    private String address;
+    private String bankName;
+    private String countryISO2;
+    private String CountryName;
+    private boolean isHeadquarter;
+    private String swiftCode;
+}
---
+++ src/main/java/org/example/SwiftCode.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class SwiftCode {
+    private String swiftCode;
+    private String bankName;
+    private String address;
+    private String countryISO2;
+    private String countryName;
+    private boolean isHeadquarter;
+}
